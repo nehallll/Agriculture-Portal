dd197ef6e8d210940e30fe122030c7598c1b148f
Nehal Shrivastava <nehals1674@gmail.com>
2025-02-06 13:27:30 +0530

    Dotnet code with models

--- AdminPortal/Program.cs
+++ AdminPortal/Program.cs
@@ -12,17 +12,18 @@ namespace AdminPortal
     {
         public static void Main(string[] args)
         {
+
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
-            
+            builder.Services.AddDbContext<ApplicationContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
             var app = builder.Build();
-
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
@@ -31,13 +32,7 @@ namespace AdminPortal
             }
 
             app.UseHttpsRedirection();
-
-            app.UseAuthorization();
-
-
-            app.MapControllers();
-
             app.Run();
-            }
+        }
     }
 }
