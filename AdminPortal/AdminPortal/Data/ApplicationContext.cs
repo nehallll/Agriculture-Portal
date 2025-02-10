d6fbafe834a87e3168f1dc83f0f560ab64c5135e
Vikrant Gupta <vikrantgupta7277@gmail.com>
2025-02-10 16:41:16 +0530

    Jwt added in dotnet

---
+++ AdminPortal/AdminPortal/Data/ApplicationContext.cs
@@ -0,0 +1,24 @@
+﻿using System.Security.Cryptography.X509Certificates;
+using AdminPortal.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AdminPortal.Data
+{
+    public class ApplicationContext : DbContext
+    {
+        public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options)
+        { }
+        public DbSet<Admin> Admin { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Admin>().Property(a=>a.Id).ValueGeneratedOnAdd();
+
+            modelBuilder.Entity<Admin>().Property(a => a.CreatedAt).HasDefaultValueSql("GETDATE()");
+
+            modelBuilder.Entity<Admin>().Property(a => a.UpdatedAt).HasDefaultValueSql("GETDATE()");
+        }
+
+    }
+}
