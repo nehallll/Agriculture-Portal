e340013b2e569abe244d3d96d9dc4847f59239cf
AaryanTrivedi <aaryantrivedi19@gmail.com>
2025-02-10 21:04:56 +0530

    Moved Admin Backend Files

---
+++ DotnetBackend/Data/MappingProfile.cs
@@ -0,0 +1,15 @@
+﻿using AdminPortal.Dto;
+using AdminPortal.Models;
+using AutoMapper;
+
+namespace AdminPortal.Data
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<RegisterRequestDto, Admin>()
+                .ForMember(dest => dest.Salt, opt => opt.Ignore());
+        }
+    }
+}
