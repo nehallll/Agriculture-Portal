90ed54a6be93409a07a94e575c5f151f442097d7
vikrant-gupta <vikrantgupta7277@gmail.com>
2025-02-10 18:42:41 +0000

    Farming types and product types added

---
+++ agri-portal-react/src/productTypes.js
@@ -0,0 +1,29 @@
+const productTypes = [
+    "grains",
+    "pulses",
+    "fruits",
+    "vegetables",
+    "nuts",
+    "tubers",
+    "herbs",
+    "spices",
+    "oilseeds",
+    "sugarcane",
+    "beverages",
+    "forage",
+    "fiber",
+    "rubber",
+    "medicinals",
+    "timber",
+    "biofuel",
+    "meat",
+    "dairy",
+    "poultry",
+    "honey",
+    "silk",
+    "seafood",
+    "organic"
+  ];
+  
+export default productTypes;
+  
